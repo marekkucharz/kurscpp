0f86d7b2ea96528d19a09579526d5bb13d1b74d0
marekkucharz <stulkakonrad2@gmail.com>
2025-10-18 12:32:18 +0200

    Create poletrojkat.cpp

---
+++ poletrojkat.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+	double a, h, Pole;
+
+	cout << "Podaj a:" << endl;
+	cin >> a;
+	cout << "Podaj h:" << endl;
+	cin >> h;
+	Pole = (a * h) / 2;
+
+	cout << "Pole:" << Pole;
+
+	  return 0;
+}
