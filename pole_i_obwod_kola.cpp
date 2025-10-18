df125f2e3804126bfaf154913a6b22f19d824489
marekkucharz <stulkakonrad2@gmail.com>
2025-10-18 12:44:33 +0200

    Create pole_i_obwod_kola.cpp

---
+++ pole_i_obwod_kola.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+	double r, pi, Obwod, Pole;
+
+	cout << "Podaj r:" << endl;
+	cin >> r;
+	pi=3.14
+	Pole = pi*(r*r);
+	Obwod = 2 * pi * r;
+	cout << "Pole:" << Pole;
+	cout << "Obwod:" << Obwod;
+	  return 0;
+}
