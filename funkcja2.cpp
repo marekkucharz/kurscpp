d8f235b973bf5a9b8462b0c2ef474d8e7dcf883b
marekkucharz <stulkakonrad2@gmail.com>
2025-10-25 14:28:32 +0200

    Create funkcja2.cpp

---
+++ funkcja2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>      
+#include<cstring>
+#include<cmath>
+#include<cctype>
+
+using namespace std;
+
+int wypiszSume(int a, int b) {
+	return a + b;
+}
+int main() {
+	cout << wypiszSume(3, 5);
+	return 0;
+}
