1f22a2d06966e90b8fce2fdad279dbf79747a251
marekkucharz <stulkakonrad2@gmail.com>
2025-10-25 14:18:29 +0200

    Create funkcja1.cpp

---
+++ funkcja1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>      
+#include<cstring>
+#include<cmath>
+#include<cctype>
+
+using namespace std;
+
+void powitanie()
+{
+	cout << "Witaj w świecie C++!";
+
+	
+
+}
+int main() {
+	powitanie();
+
+		return 0;
+}
